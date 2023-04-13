07eebc64f64f91195af7eafbb2b17fd941193543
Raypcte <rayanevalak@gmail.com>
2023-04-13 17:30:05 -0300

    correcao do req04

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -41,11 +41,28 @@ def scrape_next_page_link(html_content):
 def scrape_news(html_content):
     """Seu código deve vir aqui"""
     selector = Selector(text=html_content)
-    link = selector.css("div.tec--list a.tec--btn::attr(href)").get()
-    if link:
-        return link
-    else:
-        return None
+
+    url = selector.css("link[rel='canonical']::attr(href)").get()
+    title = selector.css(".entry-title::text").get().strip()
+    timestamp = selector.css("li.meta-date::text").get()
+    writer = selector.css("span.author a::text").get()
+    reading_time = int(
+        selector.css(".meta-reading-time::text").get().split()[0]
+    )
+    summary = "".join(
+        selector.css(".entry-content > p:nth-of-type(1) ::text").getall()
+    ).strip()
+    category = selector.css(".category-style span.label::text").get()
+
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "reading_time": reading_time,
+        "summary": summary,
+        "category": category,
+    }
 
 
 # Requisito 5
