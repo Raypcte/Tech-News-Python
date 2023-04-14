331ee24ddfe398b04c6a5cb677f648aa7640909b
Raypcte <rayanevalak@gmail.com>
2023-04-14 12:55:22 -0300

    req07

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,17 @@
+from tech_news.database import search_news
+import noticia
+
 # Requisito 7
+
+
 def search_by_title(title):
     """Seu código deve vir aqui"""
+    regex = noticia.compile(title, noticia.I)
+    query = {"title": regex}
+    res = []
+    for news in search_news(query):
+        res.append((news["title"], news["url"]))
+    return res
 
 
 # Requisito 8
